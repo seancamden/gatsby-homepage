4ab2714168389d3cd99a7443b992dea4a22ee601
Sean Camden <14354566+seancamden@users.noreply.github.com>
2024-06-11 16:15:59 -0700

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,9 +4,9 @@ import PropTypes from 'prop-types'
 const Footer = props => (
   <footer id="footer" style={props.timeout ? { display: 'none' } : {}}>
     <p className="copyright">
-      &copy; Gatsby Starter - Dimension. Design:{' '}
+      Design:{' '}
       <a href="https://html5up.net">HTML5 UP</a>. Built with:{' '}
-      <a href="https://www.gatsbyjs.org/">Gatsby.js</a>. Some images by:{' '}
+      <a href="https://www.gatsbyjs.org/">Gatsby.js</a>. Some images from:{' '}
       <a href="https://source.unsplash.com/">Unsplash Source</a>
     </p>
   </footer>
